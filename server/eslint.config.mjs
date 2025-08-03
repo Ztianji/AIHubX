07fafc9cca1e2816dd7fb9a972318c4476884f5b
Ztianji <zhangtj20230410@gmail.com>
2025-08-03 15:23:42 +0800

    chore: add server and client setups

---
+++ server/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    ignores: ['dist', 'node_modules'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.node,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
